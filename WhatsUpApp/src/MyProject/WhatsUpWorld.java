6632d6569fc06b2d2d0dd21deb08d3cd10dd8a54
Absurdistbeezay <b_jay05@hotmail.com>
2019-01-20 17:58:37 -0500

    practice isolating parts of file path

---
+++ WhatsUpApp/src/MyProject/WhatsUpWorld.java
@@ -0,0 +1,51 @@
+package MyProject;
+
+public class WhatsUpWorld {
+
+	public static void main(String[] args) {
+		
+		String str = "Whats up World?";
+		int num1 = 1;
+		System.out.println(num1+". " + str);
+		
+//		int value1 = 1;
+//		int value2 = 2;
+		
+//		System.out.println((value1 == 1) && (value2 == 2));
+//		System.out.println((value1==1)||(value2 ==1));
+//		
+		float number = 123.567f;
+		String numberStr = Float.toString(number);
+		
+		int dotPosition = numberStr.indexOf('.');
+		int secondPart = numberStr.length() - dotPosition - 1;
+		
+		System.out.println("Number of chars before dot: " + dotPosition);
+		System.out.println("Number of chars after dot: " + secondPart);
+		
+//		System.out.println(dotPosition);
+		String name = "ram prashad";
+		System.out.println(name.substring(0, 3));
+		
+		//Write a program to isolate different parts of a file path
+		
+		String fullPath = "home/user/vijay/index.txt";
+		
+		int lastSlashIndex = fullPath.lastIndexOf('/');
+		
+		String path = fullPath.substring(0, lastSlashIndex);
+		
+		String fullFile = fullPath.substring(lastSlashIndex+ 1, fullPath.length());
+		
+		int indexOfDot = fullFile.indexOf('.');
+		
+		String fileName = fullFile.substring(0, indexOfDot);
+		String extension = fullFile.substring(indexOfDot +1, fullFile.length());
+		
+//		System.out.println("Path: " + path);
+				
+		System.out.println(extension);
+		
+		
+	}
+}
